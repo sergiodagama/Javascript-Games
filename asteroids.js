3af424ce8c6ead29072c761e74fe3d7c85793a01
henriquecscode <henriquecostasousa.hcs@gmail.com>
2018-10-31 21:58:07 +0000

    Now movement resembles space

--- asteroids.js
+++ asteroids.js
@@ -7,57 +7,80 @@ context.canvas.width = w_canvas;
 context.canvas.height = h_canvas;
 
 player = {
-    x: w_canvas/2,
-    y: h_canvas/2,
+    x: w_canvas / 2,
+    y: h_canvas / 2,
     x_velocity: 0,
     y_velocity: 0,
     velocity: 0,
+    max_velocity: 4,
 
-    turning_speed: 5/360 * 2*Math.PI, //Affects the speed with which the player turns
-    forward_speed: 10,
+    turning_speed: 5 / 360 * 2 * Math.PI, //Affects the speed with which the player turns
+    forward_speed: 0.3,
+    velocity_direction: 0,
     direction: 0, //Angle between 0 and 2PI
 
     player_wingspan: 16,
     player_height: 16,
 
-    print: function(){
+    print: function () {
         context.fillStyle = "#0000ff";
 
         //Draws the player
         context.translate(player.x, player.y); //Makes the coords relative to the center of the player
         context.rotate(player.direction); //Rotates the path that is going to be made
         context.beginPath();
-        context.lineTo(-player.player_wingspan/2, player.player_height/2);
-        context.lineTo(0, -player.player_height/2);
-        context.lineTo(player.player_wingspan/2, player.player_height/2);
-        context.lineTo(0, player.player_height/4);
-        context.lineTo(-player.player_wingspan/2, player.player_height/2);
+        context.lineTo(-player.player_wingspan / 2, player.player_height / 2);
+        context.lineTo(0, -player.player_height / 2);
+        context.lineTo(player.player_wingspan / 2, player.player_height / 2);
+        context.lineTo(0, player.player_height / 4);
+        context.lineTo(-player.player_wingspan / 2, player.player_height / 2);
         context.fill();
         context.rotate(-player.direction); //Cancels the rotation
         context.translate(-player.x, -player.y); //Makes the coords absolute again
     },
 
-    movement: function(){
-        console.log("movement", player.x + " " + player.y);
-        if(controller.forward) player.velocity += player.forward_speed;
-        if(controller.left){
-            player.direction -= player.turning_speed;
-            if(player.direction < 0){
-                player.direction += 2*Math.PI;
-            }
-        }
-        if(controller.right) {
-            player.direction += player.turning_speed;
-            if(player.direction > 2*Math.PI){
-                player.direction -= 2*Math.PI;
-            }
-        }
-    
-        player.y_velocity = player.velocity * -Math.cos(player.direction);
-        player.x_velocity = player.velocity * Math.sin(player.direction);
+    movement: function () {
+        //console.log("movement", player.x + " " + player.y);
+        if (controller.forward) {
+            let last_y_velocity = player.velocity * -Math.cos(player.velocity_direction);
+            let last_x_velocity = player.velocity * Math.sin(player.velocity_direction);
+            let now_y_velocity = player.forward_speed * -Math.cos(player.direction);
+            let now_x_velocity = player.forward_speed * Math.sin(player.direction);
+            let y_velocity = last_y_velocity + now_y_velocity;
+            let x_velocity = last_x_velocity + now_x_velocity;
 
-        player.velocity *= 0.1; //Friction to prevent infinite growth
-    
+            //console.log(last_x_velocity, last_y_velocity, now_x_velocity, now_y_velocity, x_velocity, y_velocity);
+
+            player.velocity = Math.sqrt(Math.pow(x_velocity, 2) + Math.pow(y_velocity, 2));
+
+            if (x_velocity > 0) {
+                player.velocity_direction = Math.atan(y_velocity / x_velocity) + Math.PI / 2;
+            }
+            else {
+                player.velocity_direction = Math.atan(y_velocity / x_velocity) + 3 * Math.PI / 2;
+            }
+            console.log("direction", player.velocity_direction);
+        }
+        if (controller.left) {
+            player.direction -= player.turning_speed;
+            if (player.direction < 0) {
+                player.direction += 2 * Math.PI;
+            }
+        }
+        if (controller.right) {
+            player.direction += player.turning_speed;
+            if (player.direction > 2 * Math.PI) {
+                player.direction -= 2 * Math.PI;
+            }
+        }
+
+        player.y_velocity = player.velocity * -Math.cos(player.velocity_direction);
+        player.x_velocity = player.velocity * Math.sin(player.velocity_direction);
+
+        if (player.velocity > player.max_velocity) {
+            player.velocity -= player.forward_speed; //Friction to prevent infinite growth
+        }
+
         player.x = (player.x + w_canvas + player.x_velocity) % w_canvas;
         player.y = (player.y + h_canvas + player.y_velocity) % h_canvas;
     }
@@ -76,28 +99,28 @@ controller = {
     keyListener: function (event) {
         var key_state = event.type == "keydown" ? true : false;
 
-		switch (event.keyCode) {
-			case 65: //a key
+        switch (event.keyCode) {
+            case 65: //a key
                 controller.left = key_state;
-				break;
-			case 87: //w key
-				controller.forward = key_state;
-				break;
-			case 68: //d key
-				controller.right = key_state;
-				break;
+                break;
+            case 87: //w key
+                controller.forward = key_state;
+                break;
+            case 68: //d key
+                controller.right = key_state;
+                break;
         }
     }
-    
+
 }
 
 
 
-function loop(){
+function loop() {
     context.fillStyle = "#000000";
-    context.fillRect(0,0,w_canvas, h_canvas);
+    context.fillRect(0, 0, w_canvas, h_canvas);
 
-    console.log(player.direction);
+    //console.log(player.direction);
 
     player.movement();
     player.print();
